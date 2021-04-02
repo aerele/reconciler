0aa9ba13e42f8fc9eb811946ac8cee80ed7c18c3
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-04-02 12:08:03 +0530

    matching tool script report updated

---
+++ reconciler/reconciler/report/cr_gstr_2a_vs_pr_gstin_matching_tool/cr_gstr_2a_vs_pr_gstin_matching_tool.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Aerele Technologies Private Limited and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["CR GSTR 2A vs PR GSTIN Matching Tool"] = {
+	"filters": [
+	]
+};
