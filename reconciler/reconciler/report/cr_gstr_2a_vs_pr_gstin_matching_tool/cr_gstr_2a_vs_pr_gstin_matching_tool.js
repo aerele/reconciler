3a6bcefa83fe7f6c3c26567b11ad6d1b193a081c
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-04-05 01:41:58 +0530

    dialog view and bulk update functionality added

--- reconciler/reconciler/report/cr_gstr_2a_vs_pr_gstin_matching_tool/cr_gstr_2a_vs_pr_gstin_matching_tool.js
+++ reconciler/reconciler/report/cr_gstr_2a_vs_pr_gstin_matching_tool/cr_gstr_2a_vs_pr_gstin_matching_tool.js
@@ -133,15 +133,235 @@ frappe.query_reports["CR GSTR 2A vs PR GSTIN Matching Tool"] = {
 	onload: function(query_report) {
 	query_report.page.clear_menu();
 	query_report.page.add_action_item(__("✔️ Accept system values"), () => {
+		var selected_rows = [];
+		let is_selected = false;
+			$('.dt-scrollable').find(":input[type=checkbox]").each((idx, row) => {
+				if(row.checked){
+					is_selected = true;
+					selected_rows.push({'gstr_2a':frappe.query_report.data[idx]['gstr_2a']});
+				}
+			});
+			if(is_selected == false)
+			{
+				frappe.throw(__("Please select rows to update status"));
+				return
+			}
+			else{
+			frappe.call('reconciler.reconciler.report.cr_gstr_2a_vs_pr_gstin_matching_tool.cr_gstr_2a_vs_pr_gstin_matching_tool.update_status', {
+				data: selected_rows,
+				status: 'Accept system values',
+				freeze: true
+			}).then(r => {
+				frappe.msgprint(__("Status Updated"));
+			});
+		}
 	})
 	query_report.page.add_action_item(__("✔️ Accept gstr2a values"), () => {
+		var selected_rows = [];
+		let is_selected = false;
+			$('.dt-scrollable').find(":input[type=checkbox]").each((idx, row) => {
+				if(row.checked){
+					is_selected = true;
+					selected_rows.push({'gstr_2a':frappe.query_report.data[idx]['gstr_2a']});
+				}
+			});
+			if(is_selected == false)
+			{
+				frappe.throw(__("Please select rows to update status"));
+				return
+			}
+			else{
+			frappe.call('reconciler.reconciler.report.cr_gstr_2a_vs_pr_gstin_matching_tool.cr_gstr_2a_vs_pr_gstin_matching_tool.update_status', {
+				data: selected_rows,
+				status: 'Accept gstr2a values',
+				freeze: true
+			}).then(r => {
+				frappe.msgprint(__("Status Updated"));
+			});
+		}
 	})
 	query_report.page.add_action_item(__("⌛Pending"), () => {
+		var selected_rows = [];
+		let is_selected = false;
+			$('.dt-scrollable').find(":input[type=checkbox]").each((idx, row) => {
+				if(row.checked){
+					is_selected = true;
+					selected_rows.push({'gstr_2a':frappe.query_report.data[idx]['gstr_2a']});
+				}
+			});
+			if(is_selected == false)
+			{
+				frappe.throw(__("Please select rows to update status"));
+				return
+			}
+			else{
+			frappe.call('reconciler.reconciler.report.cr_gstr_2a_vs_pr_gstin_matching_tool.cr_gstr_2a_vs_pr_gstin_matching_tool.update_status', {
+				data: selected_rows,
+				status: 'Pending',
+				freeze: true
+			}).then(r => {
+				frappe.msgprint(__("Status Updated"));
+			});
+		}
 	})
 }
 }
-var render = function(statuswise_count, dialog) {
+var render = function(tax_details, other_details, dialog) {
+	let tax_details_summary = () => {
+		let summary = ``
+		$.each(tax_details, function(i, d) {
+			summary+=`
+				<tr>
+					<td>${i}</td>`
+			for (let key in d) {
+				summary+=`
+					<td>${d[key]}</td>`
+			}
+			summary+=`</tr>`
+		});
+		return `
+			<div>
+				<table class="table table-bordered">
+					<tr>
+						<th width="20%">${__('Data Source')}</th>
+						<th width="10%">${__('Taxable Value')}</th>
+						<th width="10%">${__('Tax Value')}</th>
+						<th width="10%">${__('IGST')}</th>
+						<th width="10%">${__('CGST')}</th>
+						<th width="10%">${__('SGST')}</th>
+						<th width="10%">${__('CESS')}</th>
+					</tr>
+					${summary}
+				</table>
+			</div>
+		`;
+	}
+
+	let other_details_summary = () => {
+		let summary = ``
+		$.each(other_details, function(i, d) {
+			summary+=`
+				<tr>
+					<td>${i}</td>`
+			for (let key in d) {
+				summary+=`
+					<td>${d[key]}</td>`
+			}
+			summary+=`</tr>`
+		});
+		return `
+			<div>
+				<table class="table table-bordered">
+					<tr>
+						<th width="20%">${__('Data Source')}</th>
+						<th width="10%">${__('Doc No')}</th>
+						<th width="10%">${__('Date')}</th>
+						<th width="10%">${__('POS')}</th>
+						<th width="10%">${__('Reverse Charge')}</th>
+					</tr>
+					${summary}
+				</table>
+			</div>
+		`;
+	}
+	let html = `
+		${tax_details_summary()}
+		${other_details_summary()}
+	`;
+
+	dialog.get_field('preview_html').html(html);
 }
 
-var set_listeners= function() {
+var update_status= function(gstr2a, purchase_inv) {
+	var dialog = new frappe.ui.Dialog({
+		title: __("Selection Summary"),
+		fields: [
+			{
+				"label": "Preview",
+				"fieldname": "preview_html",
+				"fieldtype": "HTML"
+			},
+			{
+				"label": "Action",
+				"fieldname": "action",
+				"fieldtype": "Select",
+				"reqd": 1,
+				"options": ['Accept System Value', 'Accept GSTR2A Value', 'Pending']
+			}
+		],
+		primary_action: function() {
+			frappe.call('reconciler.reconciler.report.cr_gstr_2a_vs_pr_gstin_matching_tool.cr_gstr_2a_vs_pr_gstin_matching_tool.update_status', {
+				data: [{'gstr_2a': gstr2a}],
+				status: dialog.fields_dict.action.value
+			}).then(r => {
+				frappe.msgprint(__("Status Updated"));
+			});
+			dialog.hide();
+		},
+		primary_action_label: __('Update'),
+	});
+	frappe.call('reconciler.reconciler.report.cr_gstr_2a_vs_pr_gstin_matching_tool.cr_gstr_2a_vs_pr_gstin_matching_tool.get_selection_details', {
+		gstr2a: gstr2a,
+		purchase_inv: purchase_inv,
+		show_tax: 1,
+		freeze: true
+	}).then(r => {
+		this.render(r.message[0], r.message[1], dialog);
+	});
+	dialog.get_field('preview_html').html('Loading...');
+	dialog.show();
+	dialog.$wrapper.find('.modal-dialog').css("width", "800px");
+}
+
+var create_purchase_inv= function(gstr2a, purchase_inv) {
+	var dialog = new frappe.ui.Dialog({
+		title: __("Selection Summary"),
+		fields: [
+			{
+				"label": "Preview",
+				"fieldname": "preview_html",
+				"fieldtype": "HTML"
+			}
+		],
+		primary_action: function() {
+			frappe.show_progress('Creating Purchase Invoice..', 70, 100, 'Please wait');
+			frappe.call({
+				method: "frappe.client.get",
+				args: {
+					doctype: "CD GSTR 2A Entry",
+					name: gstr2a,
+				},
+				callback(r) {
+					if(r.message) {
+						var gstr2a_doc = r.message;
+						frappe.new_doc("Purchase Invoice",{"company": gstr2a_doc.cf_company},
+						doc => {doc.bill_no = gstr2a_doc.cf_invoice_number,
+						doc.supplier = frappe.query_report.get_filter_value('cf_supplier'),
+						doc.bill_date = gstr2a_doc.cf_invoice_date,
+						doc.reverse_charge = gstr2a_doc.cf_reverse_charge, 
+						doc.gst_category = gstr2a_doc.cf_invoice_type,
+						doc.place_of_supply = gstr2a_doc.cf_place_of_supply,
+						doc.grand_total = gstr2a_doc.cf_invoice_amount,
+						doc.taxes_and_charges_added = gstr2a_doc.cf_tax_amount,
+						doc.total = gstr2a_doc.cf_taxable_amount,
+						doc.supplier_gstin = gstr2a_doc.party_gstin,
+						doc.company_gstin = gstr2a_doc.company_gstin});
+					}
+				}
+			});
+			dialog.hide();
+		},
+		primary_action_label: __('Create')
+	});
+	frappe.call('reconciler.reconciler.report.cr_gstr_2a_vs_pr_gstin_matching_tool.cr_gstr_2a_vs_pr_gstin_matching_tool.get_selection_details', {
+		gstr2a: gstr2a,
+		purchase_inv: purchase_inv,
+		show_tax: 1,
+		freeze: true
+	}).then(r => {
+		this.render(r.message[0], r.message[1], dialog);
+	});
+	dialog.get_field('preview_html').html('Loading...');
+	dialog.show();
+	dialog.$wrapper.find('.modal-dialog').css("width", "800px");
 }
