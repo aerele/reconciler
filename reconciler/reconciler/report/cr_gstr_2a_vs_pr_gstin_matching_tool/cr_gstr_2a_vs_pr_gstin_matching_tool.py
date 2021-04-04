ec67b84fd0e7f42abd138fd00405ab508b1a864c
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-04-05 01:49:03 +0530

    functions updated for bulk update status, selection summary. report summary

--- reconciler/reconciler/report/cr_gstr_2a_vs_pr_gstin_matching_tool/cr_gstr_2a_vs_pr_gstin_matching_tool.py
+++ reconciler/reconciler/report/cr_gstr_2a_vs_pr_gstin_matching_tool/cr_gstr_2a_vs_pr_gstin_matching_tool.py
@@ -2,8 +2,9 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+import frappe, json
 from erpnext.regional.india.utils import get_gst_accounts
+from six import string_types
 
 def execute(filters=None):
 	return MatchingTool(filters).run()
@@ -57,6 +58,18 @@ class MatchingTool(object):
 						"width": 200
 					},
 					{
+						"label": "GSTR 2A - Invoice Number",
+						"fieldname": "gstr_2a_inv",
+						"fieldtype": "Data",
+						"width": 200
+					},
+					{
+						"label": "PR - Invoice Number",
+						"fieldname": "pr_inv",
+						"fieldtype": "Data",
+						"width": 200
+					},
+					{
 						"label": "Match Status",
 						"fieldname": "match_status",
 						"fieldtype": "Data",
@@ -77,26 +90,29 @@ class MatchingTool(object):
 
 	def get_data(self):
 		data = []
+		status = [None, 'Pending']
 		if self.filters and self.filters['cf_document_type'] == 'Invoices':
 			if self.filters['cf_view_type'] == 'Supplier View':
-				gstr2a_entries = frappe.db.get_all('CD GSTR 2A Entry', filters=[['cf_invoice_date' ,'>=',self.filters['cf_from_date']],
+				gstr2a_entries = frappe.db.get_all('CD GSTR 2A Entry', filters=[
+				['cf_status' ,'in', status],
+				['cf_invoice_date' ,'>=',self.filters['cf_from_date']],
 				['cf_invoice_date' ,'<=',self.filters['cf_to_date']],
-				['cf_company_gstin', '=', self.filters['cf_company_gstin']]], fields =['cf_party_gstin','cf_tax_amount'])
+				['cf_company_gstin', '=', self.filters['cf_company_gstin']]], fields =['cf_party_gstin','cf_tax_amount', 'cf_party'])
 				
 				pi_entries = frappe.db.get_all('Purchase Invoice', filters=[['bill_date' ,'>=',self.filters['cf_from_date']],
 				['bill_date' ,'<=',self.filters['cf_to_date']],
-				['company_gstin', '=', self.filters['cf_company_gstin']]], fields =['supplier_gstin','taxes_and_charges_added'])
+				['company_gstin', '=', self.filters['cf_company_gstin']]], fields =['supplier_gstin','taxes_and_charges_added', 'supplier'])
 
 				supplier_data_by_2a = {}
 				for entry in gstr2a_entries:
 					if not entry['cf_party_gstin'] in supplier_data_by_2a:
-						supplier_data_by_2a[entry['cf_party_gstin']] = [get_supplier_by_gstin(entry['cf_party_gstin']), entry['cf_tax_amount'], 0]
+						supplier_data_by_2a[entry['cf_party_gstin']] = [entry['cf_party'], entry['cf_tax_amount'], 0]
 					else:
 						supplier_data_by_2a[entry['cf_party_gstin']][1] += entry['cf_tax_amount']
 				
 				for entry in pi_entries:
 					if not entry['supplier_gstin'] in supplier_data_by_2a:
-						supplier_data_by_2a[entry['supplier_gstin']] = [get_supplier_by_gstin(entry['supplier_gstin']), 0, entry['taxes_and_charges_added']]
+						supplier_data_by_2a[entry['supplier_gstin']] = [entry['supplier'], 0, entry['taxes_and_charges_added']]
 					else:
 						supplier_data_by_2a[entry['supplier_gstin']][2] += entry['taxes_and_charges_added']
 
@@ -113,43 +129,62 @@ class MatchingTool(object):
 					match_status = [self.filters['cf_match_status']]
 				
 				gstr2a_entries = frappe.db.get_all('CD GSTR 2A Entry', filters=[['cf_invoice_date' ,'>=',self.filters['cf_from_date']],
+				['cf_status' ,'in', status],
+				['cf_party', '=', self.filters['cf_supplier']],
 				['cf_invoice_date' ,'<=',self.filters['cf_to_date']],
 				['cf_match_status','in', match_status],
 				['cf_company_gstin', '=', self.filters['cf_company_gstin']]], fields =['cf_invoice_number','cf_party_gstin', 'cf_purchase_invoice', 'cf_match_status', 'cf_reason', 'name'])
 
 				for entry in gstr2a_entries:
 					linked_inv.add(entry['cf_purchase_invoice'])
-					if get_supplier_by_gstin(entry['cf_party_gstin']) == self.filters['cf_supplier']:
-						button = f"""<Button class="btn btn-primary btn-xs center"  gstr2a = {entry["name"]} pi ={entry["cf_purchase_invoice"]} onClick='set_listeners(this.getAttribute("gstr2a"), this.getAttribute("pi"))'>View</a>"""
-						bill_no = frappe.db.get_value("Purchase Invoice", entry['cf_purchase_invoice'], 'bill_no')
-						data.append({'gstr_2a': entry['cf_invoice_number'], 
-						'pr': bill_no, 
-						'match_status': entry['cf_match_status'], 
-						'reason':entry['cf_reason'],
-						'view': button})
+					button = f"""<Button class="btn btn-primary btn-xs center"  gstr2a = {entry["name"]} purchase_inv ={entry["cf_purchase_invoice"]} onClick='update_status(this.getAttribute("gstr2a"), this.getAttribute("purchase_inv"))'>View</a>"""
+					if 'Missing in PR' == entry['cf_match_status']:
+						button = f"""<Button class="btn btn-primary btn-xs center"  gstr2a = {entry["name"]} purchase_inv ={entry["cf_purchase_invoice"]} onClick='create_purchase_inv(this.getAttribute("gstr2a"), this.getAttribute("purchase_inv"))'>View</a>"""
+					bill_no = frappe.db.get_value("Purchase Invoice", entry['cf_purchase_invoice'], 'bill_no')
+					data.append({
+					'gstr_2a': entry['name'],
+					'pr': entry['cf_purchase_invoice'],
+					'gstr_2a_inv': entry['cf_invoice_number'], 
+					'pr_inv': bill_no, 
+					'match_status': entry['cf_match_status'], 
+					'reason':entry['cf_reason'],
+					'view': button})
 
 				if 'Missing in 2A' in match_status:
 					pi_entries = frappe.db.get_all('Purchase Invoice', filters=[['bill_date' ,'>=',self.filters['cf_from_date']],
 					['bill_date' ,'<=',self.filters['cf_to_date']],
+					['supplier' ,'=',self.filters['cf_supplier']],
 					['company_gstin', '=', self.filters['cf_company_gstin']]], fields =['name', 'supplier_gstin', 'bill_no'])
 
 					for inv in pi_entries:
-						if not inv['name'] in linked_inv and \
-							get_supplier_by_gstin(inv['supplier_gstin']) == self.filters['cf_supplier']:
-								button = f"""<Button class="btn btn-primary btn-xs center"  gstr2a = '' pi ={inv["name"]} onClick='set_listeners(this.getAttribute("gstr2a"), this.getAttribute("pi"))'>View</a>"""
-								data.append({'gstr_2a': None, 
-								'pr': inv['bill_no'], 
-								'match_status': 'Missing in 2A', 
-								'reason': None,
-								'view': button})
+						if not inv['name'] in linked_inv:
+							button = f"""<Button class="btn btn-primary btn-xs center"  gstr2a = '' purchase_inv ={inv["name"]} onClick='update_status(this.getAttribute("gstr2a"), this.getAttribute("purchase_inv"))'>View</a>"""
+							data.append({
+							'gstra_2a': None,
+							'pr': inv['name'],
+							'gstr_2a_inv': None, 
+							'pr_inv': inv['bill_no'], 
+							'match_status': 'Missing in 2A', 
+							'reason': None,
+							'view': button})
 		self.data = data
 	
 	def get_report_summary(self):
 		summary = []
-		match_status = {'Mismatch':'Red', 'Exact Match':'Green', 'Suggested':'Blue', 'Missing in 2A':'Red', 'Missing in PR':'Red'}
-		entries = frappe.db.get_all('CD GSTR 2A Entry', filters=[['cf_invoice_date' ,'>=',self.filters['cf_from_date']],
+		gstr2a_conditions = [['cf_invoice_date' ,'>=',self.filters['cf_from_date']],
 			['cf_invoice_date' ,'<=',self.filters['cf_to_date']],
-			['cf_company_gstin', '=', self.filters['cf_company_gstin']]], fields =['cf_match_status'])
+			['cf_company_gstin', '=', self.filters['cf_company_gstin']]]
+		pr_conditions = [['bill_date' ,'>=',self.filters['cf_from_date']],
+			['bill_date' ,'<=',self.filters['cf_to_date']],
+			['company_gstin', '=', self.filters['cf_company_gstin']]]
+		if 'cf_supplier' in self.filters:
+			pr_conditions.append(['supplier' ,'=',self.filters['cf_supplier']])
+			gstr2a_conditions.append(['cf_party' ,'=',self.filters['cf_supplier']])
+		match_status = {'Mismatch':'Red', 'Exact Match':'Green', 'Suggested':'Blue', 'Missing in 2A':'Red', 'Missing in PR':'Red'}
+		entries = frappe.db.get_all('CD GSTR 2A Entry', filters= gstr2a_conditions, fields =['cf_match_status', 'cf_tax_amount'])
+		pr_entries = frappe.db.get_all('Purchase Invoice', filters= pr_conditions , fields =['taxes_and_charges_added'])
+		gstr2a_tax_amt = sum([entry['cf_tax_amount'] for entry in entries if entry['cf_tax_amount']])
+		pr_tax_amt = sum([entry['taxes_and_charges_added'] for entry in pr_entries if entry['taxes_and_charges_added']])
 		for status in match_status:
 			summary.append(
 				{
@@ -159,28 +194,52 @@ class MatchingTool(object):
 				"datatype": "Float",
 			},
 			)
+		summary.append(
+				{
+				"value": len(entries),
+				"indicator": 'Green',
+				"label": 'Total GSTR 2A Docs',
+				"datatype": "Float",
+			},
+			)
+		summary.append(
+				{
+				"value": len(pr_entries),
+				"indicator": 'Green',
+				"label": 'Total PR Docs',
+				"datatype": "Float",
+			},
+			)
+		summary.append(
+				{
+				"value": gstr2a_tax_amt,
+				"indicator": 'Blue',
+				"label": 'GSTR 2A Tax Amount',
+				"datatype": "Float",
+			},
+			)
+		summary.append(
+				{
+				"value": pr_tax_amt,
+				"indicator": 'Blue',
+				"label": 'PR Tax Amount',
+				"datatype": "Float",
+			},
+			)
+		summary.append(
+				{
+				"value": abs(gstr2a_tax_amt - pr_tax_amt),
+				"indicator": 'Red',
+				"label": 'Tax Difference',
+				"datatype": "Float",
+			},
+			)
 		return summary
 
-def get_supplier_by_gstin(gstin):
-	supplier = None
-	link_name = frappe.db.sql("""select
-		`tabDynamic Link`.link_name
-	from
-		`tabAddress`, `tabDynamic Link`
-	where 
-		`tabAddress`.gstin = %(gstin)s and 
-		`tabDynamic Link`.parent = `tabAddress`.name and
-		`tabDynamic Link`.parenttype = 'Address' and
-		`tabDynamic Link`.link_doctype = 'Supplier'""", {"gstin": gstin})
-	
-	if link_name and link_name[0][0]:
-		supplier = link_name[0][0]
-	return supplier
-
 @frappe.whitelist()
-def get_selection_details(gstr2a, pi, show_tax):
-	show_tax = int(show_tax)
-	details = {}
+def get_selection_details(gstr2a, purchase_inv):
+	tax_details = {}
+	other_details = {}
 	gstr2a_doc = None
 	account_head_fields = {
 			'igst_account',
@@ -190,7 +249,7 @@ def get_selection_details(gstr2a, pi, show_tax):
 		}
 	if gstr2a:
 		gstr2a_doc = frappe.get_doc('CD GSTR 2A Entry', gstr2a)
-	pi_doc = frappe.get_doc('Purchase Invoice', pi)
+	pi_doc = frappe.get_doc('Purchase Invoice', purchase_inv)
 	gst_accounts = get_gst_accounts(pi_doc.company)
 	for row in pi_doc.taxes:
 		for accounts in gst_accounts.values():
@@ -199,38 +258,40 @@ def get_selection_details(gstr2a, pi, show_tax):
 					accounts.append(0)
 				accounts[-1]+=row.tax_amount
 	
-	if show_tax:
-		if gstr2a_doc:
-			details['GSTR-2A'] = [gstr2a_doc.cf_taxable_amount,
-								gstr2a_doc.cf_tax_amount,
-								gstr2a_doc.cf_igst_amount,
-								gstr2a_doc.cf_cgst_amount,
-								gstr2a_doc.cf_sgst_amount,
-								gstr2a_doc.cf_cess_amount]
+	if gstr2a_doc:
+		tax_details['GSTR-2A'] = [gstr2a_doc.cf_taxable_amount,
+							gstr2a_doc.cf_tax_amount,
+							gstr2a_doc.cf_igst_amount,
+							gstr2a_doc.cf_cgst_amount,
+							gstr2a_doc.cf_sgst_amount,
+							gstr2a_doc.cf_cess_amount]
+		
+		other_details['GSTR-2A'] = [gstr2a_doc.cf_invoice_number,
+							gstr2a_doc.cf_invoice_date,
+							gstr2a_doc.cf_place_of_supply,
+							gstr2a_doc.cf_reverse_charge]
 
-		pi_details = [pi_doc.total,
-						pi_doc.taxes_and_charges_added]
-		for acc in account_head_fields:
-			tax_amt = 0
-			if len(gst_accounts[acc])==3:
-				tax_amt = gst_accounts[acc][-1]
-			pi_details.append(tax_amt)
-		details['PR'] = pi_details
+	pi_details = [pi_doc.total,
+					pi_doc.taxes_and_charges_added]
+	for acc in account_head_fields:
+		tax_amt = 0
+		if len(gst_accounts[acc])==3:
+			tax_amt = gst_accounts[acc][-1]
+		pi_details.append(tax_amt)
+	tax_details['PR'] = pi_details
 
-	else:
-		if gstr2a_doc:
-			details['GSTR-2A'] = [gstr2a_doc.cf_invoice_number,
-								gstr2a_doc.cf_invoice_date,
-								gstr2a_doc.cf_place_of_supply,
-								gstr2a_doc.cf_reverse_charge]
-
-		details['PR'] = [pi_doc.bill_no,
-							pi_doc.bill_date,
-							pi_doc.place_of_supply,
-							pi_doc.reverse_charge]
-	return details
+	other_details['PR'] = [pi_doc.bill_no,
+						pi_doc.bill_date,
+						pi_doc.place_of_supply,
+						pi_doc.reverse_charge]
+	return [tax_details, other_details]
 
 @frappe.whitelist()
-def update_status(gstr2a, status):
-	frappe.db.set_value('CD GSTR 2A Entry', gstr2a, 'status', status)
+def update_status(data, status):
+	if isinstance(data, string_types):
+		data = json.loads(data)
+	data.append({'gstr_2a': 'GSTR2A-21-04-0251'})
+	for row in data:
+		if row:
+			frappe.db.set_value('CD GSTR 2A Entry', row['gstr_2a'], 'cf_status', status)
 	frappe.db.commit()
