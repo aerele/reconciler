db7972009d5682bd1c7d71e62af00da41178ae2c
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-04-08 00:14:28 +0530

    fix: update entry functionality added

--- reconciler/reconciler/doctype/cd_gstr_2a_entry/cd_gstr_2a_entry.js
+++ reconciler/reconciler/doctype/cd_gstr_2a_entry/cd_gstr_2a_entry.js
@@ -2,7 +2,21 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('CD GSTR 2A Entry', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if(!frm.doc.__islocal){ 
+			frm.add_custom_button(__("Update Entry"), function() {
+			 frm.trigger('update_entry');
+		 });}
+	},
+	update_entry: function(frm){
+		frappe.call({
+			method: "reconciler.reconciler.doctype.cd_gstr_2a_entry.cd_gstr_2a_entry.update_entry",
+			freeze: true,
+            args: {doc_name:frm.doc.name},
+			callback: function(r) {
+				frm.reload_doc();
+				frappe.msgprint(__("Updated successfully"))
+			}
+		})
+	}
 });
